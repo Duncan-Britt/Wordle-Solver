9844ee39d9c5967ee6f9fb71a87f4664bd821f64
Duncan Britt <dbru997@gmail.com>
2023-06-22 11:01:08 -0600

    Created game loop and game class skeleton.
    In folder src. Changed build.sh to compile the game loop.

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 set -xe
 
 # -Wall -Wextra -fsanitize=address
-g++ -Wall -Wextra -std=c++20 -o app Word_Comparison.cpp
+# g++ -Wall -Wextra -std=c++20 -o app Word_Comparison.cpp
+g++ -Wall -Wextra -std=c++20 -o app src/main.cpp src/font.cpp src/game.cpp 
