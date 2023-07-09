dce3b5bbeb778bae0bbfe017c47884dd04b0eacf
Ignacio Urbina Gonzalez <iurbgonz@gmail.com>
2023-07-08 22:43:33 -0600

    started working on the guess() class function, specifically removing words that were a 0 from input

--- src/game.cpp
+++ src/game.cpp
@@ -1,19 +1,64 @@
 #include "game.h"
+#include <iostream>
+#include <fstream>
 
+using std::cout;
+using std::endl;
 using std::string;
 
-void Game::guess(const string& input) const {
-  // TODO ...  
+void Game::guess(const string& input) {
+
+    //I will somehow make notInWord and inWord private members but currently there are some scoping issues with the lamdas.
+
+    //Stores letters included in the secret word.
+    std::vector<char> inWord;
+
+    //Stores letters not included in the secret word.
+    //All elements are removed after a guess is made.
+    std::vector<char> notInWord;
+
+    for (int i=0; i<5; i++) {
+        if (input[i] == '0') {
+            notInWord.push_back(currGuess[i]);
+        } else if (input[i] == '1') {
+            // TODO...
+        } else if (input[i] == '2') {
+            letterAndPosition.insert(std::make_pair(currGuess[i], i));
+        } else {
+            cout << "That is not a valid input!" << endl;
+            break;
+        }
+    }
+
+    //Removes all words from the list-words that contain letters not in the secret word.
+    words.remove_if([&notInWord](const string& str) {
+        return std::any_of(str.begin(), str.end(), [&notInWord](char ch) {
+            return std::find(notInWord.begin(), notInWord.end(), ch) != notInWord.end();
+        });
+    });
+
+    // TODO...
+
+    //currGuess = ;
 }
 
 void Game::printGuess() const {
-  // TODO ...  
+    cout << "Guess: " << currGuess << endl;
 }
 
 void Game::printCurrentGame() const {
-  // TODO ...  
+    cout << "List of Previous Guesses: " << endl;
+    for (int i=0; i<guessWords.size(); i++) {
+        cout << "Guess " << i+1 << ":" << guessWords[i] << endl;
+    }
 }
 
 Game::Game() {
-  // TODO ...
+    //Change word-list.csv to .txt file because idk how to traverse a csv file
+    string wordFile = "word-list.csv";
+    string word;
+    std::ifstream file(wordFile);
+    while (getline(file, word)) {
+        words.push_back(word);
+    }
 }
--- src/game.h
+++ src/game.h
@@ -1,7 +1,12 @@
 #ifndef GUARD_game_h
 #define GUARD_game_h
 
-#include <string>
+#include <iostream>
+#include <list>
+
+using std::string;
+using std::cout;
+using std::endl;
 
 // The game class will keep track of the state of the game, meaning
 // - what guesses have been made so far, 
@@ -10,12 +15,42 @@
 // It will also contain the logic for making a guess (the hard part!)
 class Game {
 public:
-  Game();
-  void guess(const std::string&) const; 
-  void printGuess() const; 
-  void printCurrentGame() const;
+    //Game() constructor transfers all
+    //words from "word-list.csv" to a vector.
+    Game();
+
+    //guess() eliminates the possible options
+    //given the player's clues and assigns
+    //new currGuess.
+    void guess(const string&);
+
+    //printGuess() prints currGuess.
+    void printGuess() const;
+
+    //printCurrentGame() prints all previous computer guesses.
+    void printCurrentGame() const;
 private:
-  
+    //Stores the possible words.
+    //words is a list because deletion is not computationally expensive.
+    std::list<string> words;
+
+    //Stores the words previously guessed.
+    std::vector<string> guessWords;
+
+    //The current computer guess.
+    //Assigned by function guess().
+    //Starting guess will be "crane" for simplicity.
+    string currGuess = "crane";
+
+//    //Stores letters not included in the secret word.
+//    //All elements are removed after a guess is made.
+//    std::vector<char> notInWord;
+
+//    //Stores letters included in the secret word.
+//    std::vector<char> inWord;
+
+    //Stores letters included in the secret word plus its position.
+    std::unordered_map<char,int> letterAndPosition;
 };
 
 #endif//GUARD_game_h
